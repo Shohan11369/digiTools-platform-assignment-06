4a61993f8848122c06606749888ba573351d3e95
Shohan111369 <sarkershohan346@gmail.com>
2026-03-30 00:17:17 +0800

    added navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import { FaCartPlus } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  return (
+    <div className="container mx-auto">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <a className=" text-[#4F39F6] text-xl font-bold">DigiTools</a>
         </div>
-    );
+        <div className="navbar-center  lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a href="">Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end gap-3">
+          <FaCartPlus />
+          <button>Login</button>
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
